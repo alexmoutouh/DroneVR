279fc8628ebb0c3552f4c1dc6c90b31a0ba6b5b9
unknown <baptiste.faraud@gmail.com>
2019-01-16 12:04:08 +0100

    Ajout menu + corrections

---
+++ Assets/Scene 0/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void quitter()
+    {
+        Application.Quit();
+    }
+
+    public void scene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void scene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
